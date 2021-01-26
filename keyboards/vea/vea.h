9af0367709555827c4aa96f9508a1c7291899458
mechmerlin <mechmerlin@gmail.com>
2021-01-26 10:37:29 -0800

    found right control

--- keyboards/vea/vea.h
+++ keyboards/vea/vea.h
@@ -5,16 +5,19 @@
 #define ___ KC_NO
 
 // K07 is not the Left Arrow
+// K7C is not Enter
+// K7D is not Up Arrow
+// K1C is not [
 
 #define LAYOUT( \
             K50, K51, K52, K53, K54, K55, K56,    K57, K58, K59, K5A, K5B, K5C, K6A, K7A, K6C,  \
   K61, K71, K40, K41, K42, K43, K44, K45, K46,    K47, K48, K49, K4A, K4B, K4C,       K4D, K69, \
-  K62, K72, K30, K31, K32, K33, K34, K35,         K17, K18, K19, K1A, K1B, K1C, K1D, K0D, K79, \
+  K62, K72, K30, K31, K32, K33, K34, K35,         K17, K18, K19, K1A, K1B, K1C, K1D, K0yD, K79, \
   K63, K73, K20, K21, K22, K23, K24, K25,         K27, K28, K29, K2A, K2B, K2C,       K7C, K6B, \
   K64, K74, K10, K11, K12, K13, K14, K15,      K37, K38, K39, K3A, K3B,       K3D, K7D, K7B, \
-  K65, K75, K00, K01, K02, K03, K04,           K6D,           K0A, K0B, K0C, K07, K68, K78   \
+  K65, K75, K00, K01, K02, K03, K04,           K6D,           K0A, K0B, K07, K0C, K68, K78   \
 ){ \
-  { K00,    K01,    K02,    K03,    K04,    ___,  ___,  K07,    ___,    ___,    K0A,   K0B,   K0C,   K0D }, \
+  { K00,    K01,    K02,    K03,    K04,    ___,  ___,  K07,    ___,    ___,    K0A,   K0B,   K0C,   ___ }, \
   { K10,    K11,    K12,    K13,    K14,    K15,  ___,  K17,    K18,    K19,    K1A,   K1B,   ___,   K1D }, \
   { K20,    K21,    K22,    K23,    K24,    K25,  ___,  K27,    K28,    K29,    K2A,   K2B,   K2C,   ___ }, \
   { K30,    K31,    K32,    K33,    K34,    K35,  ___,  K37,    K38,    K39,    K3A,   K3B,   ___,   K3D }, \
